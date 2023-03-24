f6d80e1a9ec5b094fd0a806456eb65f4bbdea590
Antoine THIEBAUT <aljthieb@gmail.com>
2023-03-24 13:20:17 +0100

    Création class Bullet Curve

---
+++ BossEyeProject/Assets/Scripts/SO/Bullet Curve.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class BulletCurve : ScriptableObject
+{
+    public float bulletTime;
+    public AnimationCurve bulletCurve;
+}
