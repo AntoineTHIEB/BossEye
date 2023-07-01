daa556a4c68722b1964127f8487aaab7e7aaffff
Antoine THIEBAUT <aljthieb@gmail.com>
2023-07-01 16:28:05 +0200

    Création système de SO pour shootPatterns + création d'un premier pattern test

---
+++ BossEyeProject/Assets/Scripts/SO/BulletPatternSO_scr.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BulletPattern")]
+public class BulletPatternSO_scr : ScriptableObject
+{
+    [Serializable] public class BulletImpulse
+    {
+        public float timeBeforePattern;
+        public CruveSO_scr bulletPattern;
+        public int nbBulletPerImpulse;
+        public float diffAngleBetweenBullets;
+        public float timeBetweenImpulses;
+        public float diffAngleFromPreviousImpulse;
+    }
+    public BulletImpulse[] Impulse;
+}
